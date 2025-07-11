fixed slice orientation normalization

dataloader.py
@@ -14,11 +14,11 @@ import os
 import h5py
 from tqdm import tqdm
 
-from geo_utils import normalize_plane_orientations
+from geo_utils import normalize_slice_orientation
 from sa_la_interp import interpolate_sa_segs_to_la
 from utils import normalize_image_with_percentile, mat_to_params, make_masked_coordinate_tensor, \
     compute_3d_image_gradients, compute_3d_image_hessian, compute_3d_image_hessian, to_1hot, \
-    compute_3d_image_gradients_dim_wise, temporal_median_filter, spatial_median_filter
+    compute_3d_image_gradients_dim_wise, temporal_median_filter, spatial_median_filter, nlm_denoise_multi, to_gif
 
 
 class CMRDataModule(pl.LightningDataModule):
@@ -26,7 +26,7 @@ class CMRDataModule(pl.LightningDataModule):
                  load_la_dir: str,
                  load_sa_dir: str,
                  preprocessed_store_path,
-                 replace_existing_processed=False,
+                 replace_existing_processed=True,
                  batch_size: int = 32,
                  num_coords: int = 4000,
                  num_workers: int = 0):
@@ -87,7 +87,10 @@ class CMRDataModule(pl.LightningDataModule):
         return self.train_dset.coord_size
 
     def get_max_slices(self) -> int:
-        return self.max_slices
+        dset = self.train_dset if self.train_dset else self.test_dset
+        with h5py.File(dset[0], 'r') as f:
+            shape = f['image_padded'].shape
+        return shape[1]
 
     def train_dataloader(self):
         return self._train_dataloader
@@ -165,16 +168,14 @@ class CMRDataModule(pl.LightningDataModule):
                 nib_subj = nib.load(slice_path)
                 img = nib_subj.get_fdata().squeeze()
                 img = torch.from_numpy(normalize_image_with_percentile(img))
-                img = temporal_median_filter(img[None, None], kernel_size=7)[0, 0]
-                img_uint = (img * 255).to(torch.uint8)
+                # img = nlm_denoise_multi(img, 49, 1, template_window_size=5,search_window_size=21)
+                img_uint = (img * 255).round().to(torch.uint8)
                 images.append(img_uint)
                 # Compute image gradients and Hessian
-                img_time_pad = torch.cat((img[...,-1:], img, img[...,:1]), -1)
-                img_d = compute_3d_image_gradients(img_time_pad[None, None])[0,...,1:-1]
-                img_d_time_pad = torch.cat((img_d[...,-1:], img_d, img_d[...,:1]), -1)
-                img_dd_x = compute_3d_image_gradients(img_d_time_pad[None,0:1])[0,...,1:-1]
-                img_dd_y = compute_3d_image_gradients(img_d_time_pad[None,1:2])[0,...,1:-1]
-                img_dd_t = compute_3d_image_gradients(img_d_time_pad[None,2:3])[0,...,1:-1]
+                img_d = compute_3d_image_gradients(img[None, None])[0]
+                img_dd_x = compute_3d_image_gradients(img_d[None,0:1])[0]
+                img_dd_y = compute_3d_image_gradients(img_d[None,1:2])[0]
+                img_dd_t = compute_3d_image_gradients(img_d[None,2:3])[0]
                 img_dd = torch.cat((img_dd_x[0:3], img_dd_y[1:3], img_dd_t[2:3]), 0)  # (dxx, dxy, dxz, dyy, dyz, dzz)
                 img_d = img_d.moveaxis(0, -1)
                 img_dd = img_dd.moveaxis(0, -1)
@@ -196,7 +197,7 @@ class CMRDataModule(pl.LightningDataModule):
                 segs.append(seg)
 
             # Normalize orientation of planes and store the 6 aff params
-            # affines = normalize_plane_orientations(affines, segs)
+            affines = normalize_slice_orientatfixedion(affines, segs)
             flippings = []
             aff_params = []
             coord_max = []

geo_utils.py
@@ -1,8 +1,13 @@
 import math
+import os
+from pathlib import Path
 from typing import Tuple, List, Optional
 
 import numpy as np
 import torch
+
+from data_utils import array_to_nifti
+from sa_la_interp import interpolate_sa_segs_to_la
 
 
 def batch_normalize_vector(vec: torch.Tensor) -> torch.Tensor:
@@ -98,10 +103,9 @@ def rotation_matrix(theta, axis):
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by theta radians.
     """
-    axis = np.asarray(axis)
-    axis = axis / math.sqrt(np.dot(axis, axis))
-    a = math.cos(theta / 2.0)
-    b, c, d = -axis * math.sin(theta / 2.0)
+    axis = axis / torch.linalg.norm(axis)
+    a = torch.cos(theta / 2.0)
+    b, c, d = -axis * torch.sin(theta / 2.0)
     aa, bb, cc, dd = a * a, b * b, c * c, d * d
     bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
     return torch.tensor([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
@@ -109,26 +113,26 @@ def rotation_matrix(theta, axis):
                          [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]], dtype=torch.float32)
 
 
-def normalize_plane_orientations(affines: List[torch.Tensor], segs: Optional[List[torch.Tensor]] = None) -> List[torch.Tensor]:
+def normalize_slice_orientation(affines: List[torch.Tensor], segs: Optional[List[torch.Tensor]] = None, debug=False) -> List[torch.Tensor]:
     """ We find landmarks in ED frame, first 3 slices are LA slices """
     ED_frame = 0
     if segs is not None:
         # If no segmentations provided, we work with top-most and bottom-most SA slices
         lv_basal_slice = 3
-        lv_apex_slice = len(affines)-1
+        lv_apex_slice = len(affines) - 1
     else:
         # Iterate from base to apex until we find a LV_pool segmentation
         lv_basal_slice = None
         for i in range(len(segs)):
-            s = segs[3+i][ED_frame]
+            s = segs[3 + i][ED_frame]
             if (s == 1).any():
-                lv_basal_slice = 3+i
+                lv_basal_slice = 3 + i
                 break
             if i > 3:
                 raise ValueError("Heart base is weirdly low!")
         # Iterate from apex to base until we find a LV_pool segmentation
         lv_apex_slice = None
-        for i in range(len(segs)-1, 3, -1):
+        for i in range(len(segs) - 1, 3, -1):
             s = segs[i][ED_frame]
             if (s == 1).any():
                 lv_apex_slice = i
@@ -140,34 +144,82 @@ def normalize_plane_orientations(affines: List[torch.Tensor], segs: Optional[Lis
         if torch.sum(segs[lv_basal_slice][ED_frame] == 1) < base_seg_thresh:
             # We do +1 because basal slice is not always properly segmented
             lv_basal_slice = lv_basal_slice + 1
-    # Find LA vector and where it intersects with middle SA slice
+    # Find long-axis (la) vector and where it intersects with middle short-axis (sa) slice. LV=Left ventricle, RV=Right ventricle
     lv_midventr_slice = (lv_apex_slice + lv_basal_slice) // 2
     plane_equations = get_image_plane_from_affine(torch.stack(affines))
     la_vector_points = plane_intersection(plane_equations[2][None], plane_equations[0][None])[0]
     la_vector = la_vector_points[1] - la_vector_points[0]
     lv_rv_vector_points = plane_intersection(plane_equations[2][None], plane_equations[lv_midventr_slice][None])[0]
     lv_rv_vector = lv_rv_vector_points[1] - lv_rv_vector_points[0]
-    w_lv_midway_center = plane_line_intersection(la_vector_points[1], la_vector_points[0], plane_equations[lv_midventr_slice])
-
+    w_lv_midway_center = plane_line_intersection(la_vector_points[1], la_vector_points[0],
+                                                 plane_equations[lv_midventr_slice])
     # Calculate affine matrix that aligns LA vector with z axis
     z_vector = torch.tensor((0, 0, 1), dtype=la_vector.dtype)
-    la_to_z_angle = torch.arccos(torch.dot(la_vector, z_vector) / la_vector.norm())  # the angle to z axis
+    la_to_z_angle = torch.arccos(torch.dot(la_vector / la_vector.norm(), z_vector).clip(-1., 1.))  # the angle to z axis
     la_rot_axis = torch.cross(z_vector, la_vector)
+    la_rot_axis = la_rot_axis / la_rot_axis.norm()
     rot_la = rotation_matrix(la_to_z_angle, la_rot_axis)
-
     # Calculate affine matrix that aligns LV-RV vector with y axis
     y_vector = torch.tensor((0, 1, 0), dtype=lv_rv_vector.dtype)
     rv_la_cross_vector = torch.cross(la_vector, lv_rv_vector) @ rot_la
-    la_to_y_angle = torch.arccos(np.dot(rv_la_cross_vector, y_vector) / rv_la_cross_vector.norm())  # the angle to y axis
+    rv_la_cross_vector = rv_la_cross_vector / rv_la_cross_vector.norm()
+    la_to_y_angle = torch.arccos(
+        torch.dot(rv_la_cross_vector / rv_la_cross_vector.norm(), y_vector).clip(-1., 1.))  # the angle to y axis
     rot_rv = rotation_matrix(la_to_y_angle, z_vector)
 
-    # Join rotation matrices and translate to origin
-    rot = rot_la @ rot_rv
-    translation = w_lv_midway_center @ rot
-    normalization_aff = torch.eye(4, dtype=la_vector.dtype)
-    normalization_aff[:3, :3] = rot
-    normalization_aff[3, :3] = -translation
-
-    oriented_affines = [aff.T @ normalization_aff for aff in affines]
-    # To map coords to world space you should left-multiply: coord @ aff
+    # First translate to put rotation center at origin
+    translate = torch.eye(4, dtype=la_vector.dtype)
+    translate[:3, 3] = -w_lv_midway_center
+    # Then rotate (inverse rotation)
+    rotation_inv = torch.eye(4, dtype=la_vector.dtype)
+    rotation_inv[:3, :3] = (rot_rv @ rot_la).T  # Transpose for inverse
+    # Then translate back
+    # post_translate = torch.eye(4, dtype=la_vector.dtype)
+    # post_translate[:3, 3] = w_lv_midway_center
+    # Combined transformation: post_translate @ rotation_inv @ pre_translate
+    normalization_aff = rotation_inv @ translate
+    oriented_affines = [normalization_aff @ aff for aff in affines]
+    if debug:
+        assert segs is not None
+        la2ch_seg = segs[0]
+        if not la2ch_seg.any():
+            la2ch_seg, _ = interpolate_sa_segs_to_la([i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in affines[3:]],
+                                                            target_shape=(segs[0].shape[0], segs[0].shape[1], segs[0].shape[-1]),
+                                                            target_aff=affines[0].numpy(),
+                                                            frames=None,
+                                                            oob_dist_thresh=10.)
+        la3ch_seg = segs[0]
+        if not la3ch_seg.any():
+            la3ch_seg, _ = interpolate_sa_segs_to_la([i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in affines[3:]],
+                                                            target_shape=(segs[1].shape[0], segs[1].shape[1], segs[1].shape[-1]),
+                                                            target_aff=affines[1].numpy(),
+                                                            frames=None,
+                                                            oob_dist_thresh=10.)
+        la4ch_seg = segs[0]
+        if not la4ch_seg.any():
+            la4ch_seg, _ = interpolate_sa_segs_to_la([i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in segs[3:]],
+                                                            [i.numpy() for i in affines[3:]],
+                                                            target_shape=(segs[2].shape[0], segs[2].shape[1], segs[2].shape[-1]),
+                                                            target_aff=affines[2].numpy(),
+                                                            frames=None,
+                                                            oob_dist_thresh=10.)
+        path = Path('alignment_debug')
+        path.mkdir(exist_ok=True)
+        array_to_nifti(str(path/f"pre_opt_la2ch.nii.gz"), la2ch_seg[:, :, None], affines[0].numpy())
+        array_to_nifti(str(path/f"pre_opt_la3ch.nii.gz"), la3ch_seg[:, :, None], affines[1].numpy())
+        array_to_nifti(str(path/f"pre_opt_la4ch.nii.gz"), la4ch_seg[:, :, None], affines[2].numpy())
+        array_to_nifti(str(path/f"pre_opt_sa3.nii.gz"), segs[5][:, :, None].numpy(), affines[5].numpy())
+        array_to_nifti(str(path/f"pre_opt_sa4.nii.gz"), segs[6][:, :, None].numpy(), affines[6].numpy())
+        array_to_nifti(str(path/f"pre_opt_sa5.nii.gz"), segs[7][:, :, None].numpy(), affines[7].numpy())
+        array_to_nifti(str(path/f"post_opt_la2ch.nii.gz"), la2ch_seg[:, :, None], oriented_affines[0].numpy())
+        array_to_nifti(str(path/f"post_opt_la3ch.nii.gz"), la3ch_seg[:, :, None], oriented_affines[1].numpy())
+        array_to_nifti(str(path/f"post_opt_la4ch.nii.gz"), la4ch_seg[:, :, None], oriented_affines[2].numpy())
+        array_to_nifti(str(path/f"post_opt_sa3.nii.gz"), segs[5][:, :, None].numpy(), oriented_affines[5].numpy())
+        array_to_nifti(str(path/f"post_opt_sa4.nii.gz"), segs[6][:, :, None].numpy(), oriented_affines[6].numpy())
+        array_to_nifti(str(path/f"post_opt_sa5.nii.gz"), segs[7][:, :, None].numpy(), oriented_affines[7].numpy())
     return oriented_affines

utils.py
@@ -1,5 +1,6 @@
 from typing import Union, Optional, Tuple, List, Dict
 
+import cv2
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -84,7 +85,7 @@ def fast_trilinear_interpolation(input_array: torch.Tensor,
 
 
 def flip_affine(affines, needs_flip):
-    # If the original affine had a determinant is <= 0, it is an umproper affine matrix and it needs to be flipped
+    # If the original affine had a determinant is <= 0, it is an improper affine matrix and it needs to be flipped
     needs_flip = needs_flip[:, None, None].repeat((1, 4, 4))
     flip = torch.eye(3, dtype=affines.dtype, device=affines.device)
     flip[0, 0] = -1
@@ -195,22 +196,30 @@ def compute_neighbourhood_matrix(coords: torch.Tensor, ref_coords: torch.Tensor,
 
 def compute_3d_image_gradients(volume):
     def get_3d_central_diff_kernel(device, dtype):
-        kernel = torch.zeros((3, 1, 3, 3, 3), device=device, dtype=dtype)
-        # Derivative along x (axis 2 -> W dimension)
-        kernel[2, 0, 1, 1, 0] = -0.5
-        kernel[2, 0, 1, 1, 2] = +0.5
-        # Derivative along y (axis 1 -> H dimension)
-        kernel[1, 0, 1, 0, 1] = -0.5
-        kernel[1, 0, 1, 2, 1] = +0.5
-        # Derivative along z (axis 0 -> D dimension)
-        kernel[0, 0, 0, 1, 1] = -0.5
-        kernel[0, 0, 2, 1, 1] = +0.5
+        kernel = torch.zeros((3, 1, 5, 5, 5), device=device, dtype=dtype)
+        # Derivative along x (axis 2 -> W)
+        kernel[2, 0, 2, 2, 0] = -1 / 12
+        kernel[2, 0, 2, 2, 1] = +8 / 12
+        kernel[2, 0, 2, 2, 3] = -8 / 12
+        kernel[2, 0, 2, 2, 4] = +1 / 12
+        # Derivative along y (axis 1 -> H)
+        kernel[1, 0, 2, 0, 2] = -1 / 12
+        kernel[1, 0, 2, 1, 2] = +8 / 12
+        kernel[1, 0, 2, 3, 2] = -8 / 12
+        kernel[1, 0, 2, 4, 2] = +1 / 12
+        # Derivative along z (axis 0 -> D)
+        kernel[0, 0, 0, 2, 2] = -1 / 12
+        kernel[0, 0, 1, 2, 2] = +8 / 12
+        kernel[0, 0, 3, 2, 2] = -8 / 12
+        kernel[0, 0, 4, 2, 2] = +1 / 12
 
-        return kernel
+        return kernel * -1
     # volume: (B, 1, D, H, W)
     device, dtype = volume.device, volume.dtype
+    volume_pad = torch.cat((volume[..., -2:], volume, volume[..., :2]), -1)
+    volume_pad = F.pad(volume_pad, (0,0,2,2,2,2))  # F.pad takes padding sequence backwards
     kernel = get_3d_central_diff_kernel(device, dtype)
-    gradients = F.conv3d(volume, kernel, padding=1)  # Pad to keep size
+    gradients = F.conv3d(volume_pad, kernel, padding=0)  # Pad to keep size
     return gradients # (B, 3, D, H, W)
 
 
@@ -310,3 +319,29 @@ def spatial_median_filter(video, kernel_size=(3, 3, 1)):
     unfolded_ = unfolded.reshape(B, C, H, W, T, -1)
     median = unfolded_.median(dim=-1).values  # (B, C, H, W, T)
     return median
+
+
+def nlm_denoise_multi(frames, temporal_window_size, h, template_window_size=None, search_window_size=None):
+    H, W, T = frames.shape
+    frames_uint8 = (frames*255.).round().clip(0., 255.).to(torch.uint8)
+    frames_uint8 = frames_uint8.numpy()
+    frames_uint8 = [frames_uint8[...,i] for i in range(T)]
+    frames_uint8_pad = frames_uint8[-(temporal_window_size//2):] + frames_uint8 + frames_uint8[:(temporal_window_size//2)]
+    frames_dn = []
+    for i in range(temporal_window_size//2, T+(temporal_window_size//2)):
+        f = cv2.fastNlMeansDenoisingMulti(frames_uint8_pad, i, temporal_window_size, h, template_window_size, search_window_size)
+        frames_dn.append(f)
+    frames_dn = np.stack(frames_dn, -1)
+    frames_dn = torch.from_numpy(frames_dn).to(torch.float32) / 255.
+    assert frames_dn.shape == (H, W, T)
+    return frames_dn
+
+
+from PIL import Image
+def to_gif(imgs, name="arr"):
+    imgs = imgs.moveaxis(-1, 0)
+    imgs = (imgs*255).round().to(torch.uint8).numpy()
+    imgs = np.stack([imgs]*3, -1)
+    imgs = [Image.fromarray(img) for img in imgs]
+    # duration is the number of milliseconds between frames; this is 40 frames per second
+    imgs[0].save(f"{name}.gif", save_all=True, append_images=imgs[1:], duration=50, loop=0)